0bda303e7a5e3efa1a2022e69594cb40ac13015f
diego <u17307196@utp.edu.pe>
2021-05-07 13:17:10 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
+app.use(express.static('./dist/Kaizen-Frontend-Main'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+    res.sendFile('index.html', {root: 'dist/Kaizen-Frontend-Main/'}),
 );
 
 // Start the app by listening on the default Heroku port
