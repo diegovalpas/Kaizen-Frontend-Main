138ded78fadf046dafa93a8e245f19ce18e111fd
diego <u17307196@utp.edu.pe>
2021-05-07 14:46:40 -0500

    d

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
+//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/kaizen-talent-app'));
+app.use(express.static(__dirname + '/dist/kaizen-talent-app'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/kaizen-talent-app/'}),
-);
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/kaizen-talent-app/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 8080);
