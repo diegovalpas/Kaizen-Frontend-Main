b4203e6223e69bd580f03339be6c6a9d8d6d5c48
diego <u17307196@utp.edu.pe>
2021-05-07 14:14:00 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/kaizen-talent-app'));
+app.use(express.static(path.join(__dirname, 'src')));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/kaizen-talent-app/'}),
