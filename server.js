365036eda865aef79641b8ab8e0d2e5e5f26d315
diego <u17307196@utp.edu.pe>
2021-05-07 14:23:21 -0500

    dsa

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
+app.set('port', process.env.PORT || 4200)
 // Serve only the static files form the dist directory
-app.use(express.static(path.join(__dirname, 'src')));
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/kaizen-talent-app/'}),
-);
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4200);
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'))
+})
